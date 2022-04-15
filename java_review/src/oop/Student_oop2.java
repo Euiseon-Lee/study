e89e1f6e4e6619e08ddee9e5694a3a2751e4cbf9
Euiseon <inia8inia@gmail.com>
2022-04-15 13:38:11 +0900

    객체 Getter Test02

--- java_review/src/oop/Student_oop2.java
+++ java_review/src/oop/Student_oop2.java
@@ -15,5 +15,23 @@ public class Student_oop2 {
 //	결과 출력 시 총점과 평균, 통과 여부를 확인할 수 있어야 함
 //	과목별 40점 이상이고 평균 60점 이상이면 통과, 아니면 재평가
 	
+	public static void main(String[] args) {
+		
+		Exam_oop2 e1 = new Exam_oop2();
+		Exam_oop2 e2 = new Exam_oop2();
+		Exam_oop2 e3 = new Exam_oop2();
+		
+		e1.setting("피카츄", "응용SW기초기술활용", 50, 60);
+		e2.setting("라이츄", "응용SW기초기술활용", 40, 80);
+		e3.setting("파이리", "프로그래밍언어활용", 60, 65);
+		
+		e1.print();
+		System.out.println("");
+		e2.print();
+		System.out.println("");
+		e3.print();
+		
+	}
+
 
 }
