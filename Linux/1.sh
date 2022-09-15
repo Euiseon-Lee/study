018d87424e9cfa2dc137fd525e1fd32056e99467
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-09-15 10:58:13 +0900

    calculator

---
+++ Linux/1.sh
@@ -0,0 +1,48 @@
+echo "I love you so much"
+echo "I hate you so much"
+echo "I want you so much"
+echo "I helped you so much"
+echo "I save you so much"
+echo "I save you so much"
+echo "I cry for you so much"
+echo "I love you so much"
+echo "I hate you so much"
+echo "I want you so much"
+echo "I helped you so much"
+echo "I save you so much"
+echo "I cry for you so much"
+echo "I love you so much"
+echo "I hate you so much"
+echo "I want you so much"
+echo "I helped you so much"
+echo "I save you so much"
+echo "I cry for you so much"
+echo "I love you so much"
+echo "I hate you so much"
+echo "I want you so much"
+echo "I helped you so much"
+echo "I save you so much"
+echo "I cry for you so much"
+echo "I love you so much"
+echo "I hate you so much"
+echo "I want you so much"
+echo "I helped you so much"
+echo "I save you so much"
+echo "I cry for you so much"
+echo "I cry for you so much"
+echo "I love you so much"
+echo "I hate you so much"
+echo "I want you so much"
+echo "I helped you so much"
+
+
+off=001.sh
+if test -f ${off};
+then
+	echo "${off} exists"
+
+else
+	echo "a variable doesn't exist"
+fi
+
+
