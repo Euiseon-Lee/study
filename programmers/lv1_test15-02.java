b41af2e50c0d9efdbf4feaa8a7dcd488b471396b
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-10-24 12:26:22 +0900

    서울에서 김서방 찾기 - String.equal

---
+++ programmers/lv1_test15-02.java
@@ -0,0 +1,14 @@
+public class FindKim {
+    public String findKim(String[] seoul){
+    //x에 김서방의 위치를 저장하세요.
+    int x = 0;
+    for(int i = 0; i < seoul.length; i++){
+        if(seoul[i].equals("Kim")){
+          x = i;
+          break;
+      }
+    }
+
+        return "김서방은 "+ x + "에 있다";
+    }
+}
