0f866420a017c69f6d77c83aa15a467b6bcab288
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-10-26 09:24:17 +0900

    제일 작은 수 제거하기 ★오래걸림
    입출력 예
    arr	        return
    [4,3,2,1]	[4,3,2]
    [10]	        [-1]

---
+++ programmers/lv1_test18.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] solution(int[] arr) {
+      /*
+      문제 설명
+        정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
+        단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 
+        예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+      제한 조건
+        arr은 길이 1 이상인 배열입니다.
+        인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+      */
+      
+        int minVal = arr[0];
+
+        if(arr.length == 1) {
+        	int[] answer = {-1};
+        	return answer;
+        }
+
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] < minVal) {
+				minVal = arr[i];
+			}
+		}
+		//System.out.println("minVal = "+minVal);
+
+        int index = 0;
+        int[] answer = new int [arr.length -1];
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] != minVal) {
+                answer[index] = arr[i];
+                index++;
+				//System.out.println("arr[i] = "+arr[i]);
+			} else {
+                continue;
+			}
+		}
+        return answer;
+	}
+}
