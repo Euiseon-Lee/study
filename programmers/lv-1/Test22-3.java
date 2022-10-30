51a4dfc50fc3bccaddfeb0aa3738ad8d35b50a7f
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-10-30 14:31:32 +0900

    수박수박수박수? - for문 + if문 + StringBuffer + toString

---
+++ programmers/lv-1/Test22-3.java
@@ -0,0 +1,28 @@
+class Solution {
+   /*
+    문제 설명
+        길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+        예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+    제한 조건
+        n은 길이 10,000이하인 자연수입니다.
+   */ 
+    
+    public String solution(int n) {
+        
+      //가장 빠른 속도를 보인 코드
+      //처리 시간 0.08ms ~ 1.36ms
+      StringBuffer repeat = new StringBuffer();
+
+      for (int i = 1; i<=n; i++) {
+        if(i%2==1) {
+          repeat.append("수");
+        } else {
+          repeat.append("박");
+        }
+      }
+
+		return repeat.toString();
+      
+    }
+}
