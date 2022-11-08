23c5a80fed43cec63882897319c244454039f950
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-11-08 17:15:07 +0900

    문자열 정렬하기 (1) - replaceAll("[^0-9]", ""); 사용
    입출력 예 #1
    "hi12392"에 있는 숫자 1, 2, 3, 9, 2를 오름차순 정렬한 [1, 2, 2, 3, 9]를 return 합니다.
    
    입출력 예 #2
    "p2o4i8gj2"에 있는 숫자 2, 4, 8, 2를 오름차순 정렬한 [2, 2, 4, 8]을 return 합니다.
    
    입출력 예 #3
    "abcde0"에 있는 숫자 0을 오름차순 정렬한 [0]을 return 합니다.

---
+++ programmers/lv-0/Test12-1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class Solution {
+    /*
+      문제 설명
+        문자열 my_string이 매개변수로 주어질 때, 
+        my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+      제한사항
+        1 ≤ my_string의 길이 ≤ 100
+        my_string에는 숫자가 한 개 이상 포함되어 있습니다.
+        my_string은 영어 소문자 또는 0부터 9까지의 숫자로 이루어져 있습니다.
+    */
+    public int[] solution(String my_string) {
+        //처리 속도 0.50 ~ 0.52ms
+        String trans = my_string.replaceAll("[^0-9]", "");
+        String trans2[] = trans.split("");
+        int[] answer = new int [trans.length()];
+        for (int i=0; i<trans.length(); i++) {
+            answer[i] = Integer.parseInt(trans2[i]);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
