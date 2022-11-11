0eab52e8234ad5f5ef5851d970aeb71b2eb0ab96
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2022-11-11 14:28:49 +0900

    가까운 수 - Array.sort()대신 조건문 추가
    입출력 예 #1
    3, 10, 28 중 20과 가장 가까운 수는 28입니다.
    
    입출력 예 #2
    10, 11, 12 중 13과 가장 가까운 수는 12입니다.

---
+++ programmers/lv-0/Test15-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    /*
+      문제 설명
+        정수 배열 array와 정수 n이 매개변수로 주어질 때,
+        array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
+
+      제한사항
+        1 ≤ array의 길이 ≤ 100
+        1 ≤ array의 원소 ≤ 100
+        1 ≤ n ≤ 100
+        가장 가까운 수가 여러 개일 경우 더 작은 수를 return 합니다.
+    */
+    public int solution(int[] array, int n) {
+        //처리 속도 0.03 ~ 0.04
+        int index = 0;
+
+        for (int i=0; i<array.length-1; i++) {
+            if(Math.abs(n-array[index]) > Math.abs(n-array[i+1])) {
+                index = i+1;
+            } else if (Math.abs(n-array[index]) < Math.abs(n-array[i+1])){
+                continue;
+            } else {
+                if(array[index] > array[i+1]) {
+                    index = i+1;
+                } else {
+                    continue;
+                }
+            }
+        }
+        return array[index];
+    }
+}
