bdaa0450651046466d320025869a6fdbb06ddeed
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2024-09-11 22:02:22 +0900

    Update Select-Lv1.sql

--- programmers/SQL-kit/Select-Lv1.sql
+++ programmers/SQL-kit/Select-Lv1.sql
@@ -1,3 +1,84 @@
+/*
+문제 설명
+ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+
+NAME	TYPE	NULLABLE
+ANIMAL_ID	VARCHAR(N)	FALSE
+ANIMAL_TYPE	VARCHAR(N)	FALSE
+DATETIME	DATETIME	FALSE
+INTAKE_CONDITION	VARCHAR(N)	FALSE
+NAME	VARCHAR(N)	TRUE
+SEX_UPON_INTAKE	VARCHAR(N)	FALSE
+
+동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 이때 결과는 ANIMAL_ID 역순으로 보여주세요.
+*/
+-- 코드를 입력하세요
+SELECT NAME, DATETIME FROM ANIMAL_INS ORDER BY ANIMAL_ID DESC;
+
+
+/*
+문제 설명
+DEVELOPER_INFOS 테이블은 개발자들의 프로그래밍 스킬 정보를 담은 테이블입니다. DEVELOPER_INFOS 테이블 구조는 다음과 같으며, ID, FIRST_NAME, LAST_NAME, EMAIL, SKILL_1, SKILL_2, SKILL_3는 각각 ID, 이름, 성, 이메일, 첫 번째 스킬, 두 번째 스킬, 세 번째 스킬을 의미합니다.
+
+NAME	TYPE	UNIQUE	NULLABLE
+ID	VARCHAR(N)	Y	N
+FIRST_NAME	VARCHAR(N)	N	Y
+LAST_NAME	VARCHAR(N)	N	Y
+EMAIL	VARCHAR(N)	Y	N
+SKILL_1	VARCHAR(N)	N	Y
+SKILL_2	VARCHAR(N)	N	Y
+SKILL_3	VARCHAR(N)	N	Y
+
+문제
+DEVELOPER_INFOS 테이블에서 Python 스킬을 가진 개발자의 정보를 조회하려 합니다. Python 스킬을 가진 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
+
+결과는 ID를 기준으로 오름차순 정렬해 주세요.
+*/
+-- 코드를 작성해주세요
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME 
+  FROM DEVELOPER_INFOS 
+ WHERE SKILL_1 || SKILL_2 || SKILL_3 LIKE '%Python%'
+ ORDER BY ID ASC;
+
+
+/*
+문제 설명
+FIRST_HALF 테이블은 아이스크림 가게의 상반기 주문 정보를 담은 테이블입니다.FIRST_HALF 테이블 구조는 다음과 같으며, SHIPMENT_ID, FLAVOR, TOTAL_ORDER는 각각 아이스크림 공장에서 아이스크림 가게까지의 출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량을 나타냅니다.
+
+NAME	TYPE	NULLABLE
+SHIPMENT_ID	INT(N)	FALSE
+FLAVOR	VARCHAR(N)	FALSE
+TOTAL_ORDER	INT(N)	FALSE
+
+문제
+상반기에 판매된 아이스크림의 맛을 총주문량을 기준으로 내림차순 정렬하고 총주문량이 같다면 출하 번호를 기준으로 오름차순 정렬하여 조회하는 SQL 문을 작성해주세요.
+*/
+-- 코드를 입력하세요
+SELECT FLAVOR
+  FROM FIRST_HALF 
+ ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC;
+
+
+/*
+문제 설명
+다음은 식품공장의 정보를 담은 FOOD_FACTORY 테이블입니다. FOOD_FACTORY 테이블은 다음과 같으며 FACTORY_ID, FACTORY_NAME, ADDRESS, TLNO는 각각 공장 ID, 공장 이름, 주소, 전화번호를 의미합니다.
+
+Column name	Type	Nullable
+FACTORY_ID	VARCHAR(10)	FALSE
+FACTORY_NAME	VARCHAR(50)	FALSE
+ADDRESS	VARCHAR(100)	FALSE
+TLNO	VARCHAR(20)	TRUE
+
+문제
+FOOD_FACTORY 테이블에서 강원도에 위치한 식품공장의 공장 ID, 공장 이름, 주소를 조회하는 SQL문을 작성해주세요. 이때 결과는 공장 ID를 기준으로 오름차순 정렬해주세요.
+*/
+-- 코드를 입력하세요
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+  FROM FOOD_FACTORY
+ WHERE ADDRESS LIKE '%강원도%'
+ ORDER BY FACTORY_ID ASC;
+
+
 /* 평균 일일 대여 요금 구하기
 문제 설명
 다음은 어느 자동차 대여 회사에서 대여중인 자동차들의 정보를 담은 CAR_RENTAL_COMPANY_CAR 테이블입니다. CAR_RENTAL_COMPANY_CAR 테이블은 아래와 같은 구조로 되어있으며, CAR_ID, CAR_TYPE, DAILY_FEE, OPTIONS 는 각각 자동차 ID, 자동차 종류, 일일 대여 요금(원), 자동차 옵션 리스트를 나타냅니다.
@@ -72,3 +153,37 @@ SELECT A.FLAVOR
  WHERE A.TOTAL_ORDER > 3000
    AND B.INGREDIENT_TYPE = 'fruit_based'
  ORDER BY A.TOTAL_ORDER DESC;
+
+
+/*
+문제 설명
+다음은 중고거래 게시판 정보를 담은 USED_GOODS_BOARD 테이블과 중고거래 게시판 첨부파일 정보를 담은 USED_GOODS_REPLY 테이블입니다. USED_GOODS_BOARD 테이블은 다음과 같으며 BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS은 게시글 ID, 작성자 ID, 게시글 제목, 게시글 내용, 가격, 작성일, 거래상태, 조회수를 의미합니다.
+
+Column name	Type	Nullable
+BOARD_ID	VARCHAR(5)	FALSE
+WRITER_ID	VARCHAR(50)	FALSE
+TITLE	VARCHAR(100)	FALSE
+CONTENTS	VARCHAR(1000)	FALSE
+PRICE	NUMBER	FALSE
+CREATED_DATE	DATE	FALSE
+STATUS	VARCHAR(10)	FALSE
+VIEWS	NUMBER	FALSE
+USED_GOODS_REPLY 테이블은 다음과 같으며 REPLY_ID, BOARD_ID, WRITER_ID, CONTENTS, CREATED_DATE는 각각 댓글 ID, 게시글 ID, 작성자 ID, 댓글 내용, 작성일을 의미합니다.
+
+Column name	Type	Nullable
+REPLY_ID	VARCHAR(10)	FALSE
+BOARD_ID	VARCHAR(5)	FALSE
+WRITER_ID	VARCHAR(50)	FALSE
+CONTENTS	VARCHAR(1000)	TRUE
+CREATED_DATE	DATE	FALSE
+
+문제
+USED_GOODS_BOARD와 USED_GOODS_REPLY 테이블에서 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회하는 SQL문을 작성해주세요. 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
+*/
+-- 코드를 입력하세요
+SELECT t1.TITLE, t1.BOARD_ID, t2.REPLY_ID, t2.WRITER_ID, t2.CONTENTS, TO_CHAR(t2.CREATED_DATE, 'YYYY-MM-DD') AS CREATED_DATE
+  FROM USED_GOODS_BOARD t1
+ INNER JOIN USED_GOODS_REPLY t2
+    ON t1.BOARD_ID = t2.BOARD_ID
+ WHERE t1.CREATED_DATE BETWEEN TO_DATE(20221001, 'YYYY-MM-DD') AND TO_DATE(20221031, 'YYYY-MM-DD')
+ ORDER BY t2.CREATED_DATE ASC, t1.TITLE ASC;
