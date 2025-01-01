b5a15b3612b9e027e9518dcb7eabafde77ba2f1a
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-01-01 21:07:30 +0900

    Update SUM_MAX_MIN.sql

--- Programmers/Sql-Kit/SUM_MAX_MIN.sql
+++ Programmers/Sql-Kit/SUM_MAX_MIN.sql
@@ -1,4 +1,4 @@
-/* LEVEL 1
+/* LEVEL 1 - 가장 비싼 상품 구하기
 문제 설명
 다음은 어느 의류 쇼핑몰에서 판매 중인 상품들의 정보를 담은 PRODUCT 테이블입니다. PRODUCT 테이블은 아래와 같은 구조로 되어있으며, PRODUCT_ID, PRODUCT_CODE, PRICE는 각각 상품 ID, 상품코드, 판매가를 나타냅니다.
 
@@ -14,7 +14,7 @@ PRODUCT 테이블에서 판매 중인 상품 중 가장 높은 판매가를 출�
 SELECT MAX(PRICE) FROM PRODUCT;
 
 
-/* LEVEL 1
+/* LEVEL 1 - 최댓값 구하기
 문제 설명
 ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
@@ -31,7 +31,7 @@ SEX_UPON_INTAKE	VARCHAR(N)	FALSE
 SELECT MAX(DATETIME) FROM ANIMAL_INS;
 
 
-/*  LEVEL 1
+/*  LEVEL 1 - 잡은 물고기 중 가장 큰 물고기의 길이 구하기
 문제 설명
 낚시앱에서 사용하는 FISH_INFO 테이블은 잡은 물고기들의 정보를 담고 있습니다. FISH_INFO 테이블의 구조는 다음과 같으며 ID, FISH_TYPE, LENGTH, TIME은 각각 잡은 물고기의 ID, 물고기의 종류(숫자), 잡은 물고기의 길이(cm), 물고기를 잡은 날짜를 나타냅니다.
 
@@ -49,4 +49,21 @@ FISH_INFO 테이블에서 잡은 물고기 중 가장 큰 물고기의 길이를
 */
 -- 코드를 작성해주세요
 SELECT CONCAT(MAX(LENGTH), 'cm') AS MAX_LENGTH FROM FISH_INFO;
+
+
+/* LEVEL 2 - 가격이 제일 비싼 식품의 정보 출력하기
+제출 내역
+문제 설명
+다음은 식품의 정보를 담은 FOOD_PRODUCT 테이블입니다. FOOD_PRODUCT 테이블은 다음과 같으며 PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE는 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을 의미합니다.
+
+Column name	Type	Nullable
+PRODUCT_ID	VARCHAR(10)	FALSE
+PRODUCT_NAME	VARCHAR(50)	FALSE
+PRODUCT_CD	VARCHAR(10)	TRUE
+CATEGORY	VARCHAR(10)	TRUE
+PRICE	NUMBER	TRUE
+문제
+FOOD_PRODUCT 테이블에서 가격이 제일 비싼 식품의 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을 조회하는 SQL문을 작성해주세요.
+*/
+
 
