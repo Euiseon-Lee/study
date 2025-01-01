49c1401b9f6081649f7b7910d10d7cb587c3e28a
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-01-01 19:03:34 +0900

    Create SUM_MAX_MIN.sql

---
+++ Programmers/Sql-Kit/SUM_MAX_MIN.sql
@@ -0,0 +1,31 @@
+/* LEVEL 1
+문제 설명
+다음은 어느 의류 쇼핑몰에서 판매 중인 상품들의 정보를 담은 PRODUCT 테이블입니다. PRODUCT 테이블은 아래와 같은 구조로 되어있으며, PRODUCT_ID, PRODUCT_CODE, PRICE는 각각 상품 ID, 상품코드, 판매가를 나타냅니다.
+
+Column name	Type	Nullable
+PRODUCT_ID	INTEGER	FALSE
+PRODUCT_CODE	VARCHAR(8)	FALSE
+PRICE	INTEGER	FALSE
+상품 별로 중복되지 않는 8자리 상품코드 값을 가지며, 앞 2자리는 카테고리 코드를 의미합니다.
+
+문제
+PRODUCT 테이블에서 판매 중인 상품 중 가장 높은 판매가를 출력하는 SQL문을 작성해주세요. 이때 컬럼명은 MAX_PRICE로 지정해주세요.
+*/
+SELECT MAX(PRICE) FROM PRODUCT;
+
+
+/* LEVEL 1
+문제 설명
+ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+
+NAME	TYPE	NULLABLE
+ANIMAL_ID	VARCHAR(N)	FALSE
+ANIMAL_TYPE	VARCHAR(N)	FALSE
+DATETIME	DATETIME	FALSE
+INTAKE_CONDITION	VARCHAR(N)	FALSE
+NAME	VARCHAR(N)	TRUE
+SEX_UPON_INTAKE	VARCHAR(N)	FALSE
+가장 최근에 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+*/
+-- 코드를 입력하세요
+SELECT MAX(DATETIME) FROM ANIMAL_INS;
