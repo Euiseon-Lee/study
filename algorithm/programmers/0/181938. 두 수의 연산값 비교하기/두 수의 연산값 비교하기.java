227045acc1c3e42049f980c5533a5c29340968d2
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-05-14 15:58:52 +0900

    Delete algorithm/programmers/0/181938. 두 수의 연산값 비교하기 directory

--- algorithm/programmers/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int solution(int a, int b) {
-        //int digitB = (int) Math.pow(10, (int) Math.log10(Math.abs(b) + 1));
-        //return Math.max((a * digitB) + b, (2 * a * b));
-        int append = Integer.parseInt("" + a + b);
-        return Math.max(append, (2 * a * b));
-    }
-}
