5ceb5e9df9886e6f777a7cc3c34d5d5948f39ae6
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-05-30 00:04:38 +0900

    Update 진료과별 총 예약 횟수 출력하기.sql
    Add annotation

--- algorithm/programmers/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ algorithm/programmers/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -1,8 +1,11 @@
--- 코드를 입력하세요
+-- 날짜 함수의 성능 이슈
+-- YEAR()와 MONTH()를 사용하면 인덱스를 무시하게 되어 성능에 영향을 줄 수 있어.
+-- BETWEEN을 쓰는 방식으로 리라이팅하면 성능에 더 유리함.
 SELECT MCDP_CD AS 진료과코드
      , COUNT(MCDP_CD) AS 5월예약건수
   FROM APPOINTMENT
- WHERE YEAR(APNT_YMD) = '2022' AND MONTH(APNT_YMD) = '5'
+ --WHERE YEAR(APNT_YMD) = 2022 AND MONTH(APNT_YMD) = 5
+ WHERE DATE(APNT_YMD) BETWEEN '2022-05-01' AND '2022-05-31'
  GROUP BY MCDP_CD
  ORDER BY 5월예약건수, 진료과코드
 ;
