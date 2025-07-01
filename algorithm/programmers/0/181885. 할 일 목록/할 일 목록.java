8d5f0add6aab7e1975f8df5cf39e146b75438519
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-07-01 22:22:03 +0900

    [level 0] Title: 할 일 목록, Time: 3.12 ms, Memory: 88.9 MB -EuiseonHub

---
+++ algorithm/programmers/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) sb.append(todo_list[i] + ",");
+        }
+        return sb.toString().split(",");
+    }
+}
