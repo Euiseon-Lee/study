a9f23f3237462f3b577504b028e575b827a53c5e
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-07-15 23:54:55 +0900

    [level 0] Title: 그림 확대, Time: 0.72 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ algorithm/programmers/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        for (int i = 0, idx = 0; i < picture.length; i++) {
+            String target = picture[i];
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < target.length(); j++) {
+                char c = target.charAt(j);
+                sb.append(Character.toString(c).repeat(k));
+            }
+            for (int j = 0; j < k; j++) {
+                answer[idx++] = sb.toString();   
+            }
+        }
+        return answer;
+    }
+}
