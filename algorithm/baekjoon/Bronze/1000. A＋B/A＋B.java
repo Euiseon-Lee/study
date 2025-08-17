cde334d1f5d06e4de2c24eb1f3267dc460743aec
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-08-17 21:52:01 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

--- algorithm/baekjoon/Bronze/1000. A＋B/A＋B.java
+++ algorithm/baekjoon/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,8 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		int a, b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a + b);
-	}
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.nextLine().split(" ");
+        System.out.println(Integer.valueOf(strArr[0]) + Integer.valueOf(strArr[1]));
+    }
 }
