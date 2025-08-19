02049f7869fa74a1d736893956f8c27465bd279c
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-08-19 20:54:39 +0900

    [Bronze IV] Title: 숫자의 합, Time: 172 ms, Memory: 17744 KB -EuiseonHub

--- algorithm/baekjoon/Bronze/11720. 숫자의 합/숫자의 합.java
+++ algorithm/baekjoon/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int answer = 0;
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
-        String[] target = sc.next().split("");
+        String s = sc.next();
         sc.close();
-        for (int i = 0; i < count; i++) {
-            answer += Integer.parseInt(target[i]);
-        }
-        System.out.println(answer);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) sum += s.charAt(i) - '0';
+        System.out.println(sum);
     }
 }
