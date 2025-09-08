06dcac06886035518c36d9d92e990603ff3a1a92
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-08 23:13:33 +0900

    [Silver IV] Title: 주몽, Time: 196 ms, Memory: 18516 KB -EuiseonHub

---
+++ algorithm/baekjoon/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int materialCount = Integer.parseInt(br.readLine());
+        int materialTotal =  Integer.parseInt(br.readLine());
+        int[] materialList = new int[materialCount];
+        Map<Integer, Integer> materials = new HashMap<Integer, Integer>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < materialCount; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            materialList[i] = target;
+            materials.put(target, materials.getOrDefault(target, 0) + 1);
+        }
+        int answer = 0;
+        for (int i = 0; i < materialCount; i++) {
+            int standard = materialList[i];
+            if (materials.getOrDefault(standard, 0) == 0) continue;
+            materials.put(standard, materials.get(standard) - 1);
+            int needed = materialTotal - standard;
+            if (materials.getOrDefault(needed, 0) > 0) {
+                answer++;
+                materials.put(needed, materials.get(needed) - 1);
+            }
+        }
+        System.out.println(answer);
+    }
+}
