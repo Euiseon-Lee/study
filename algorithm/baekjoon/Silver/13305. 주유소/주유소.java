d29e91a45109fcc942019433616139d3154386e4
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-11 23:10:21 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 30440 KB, Score: 58 point -EuiseonHub

---
+++ algorithm/baekjoon/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cityCount = Integer.parseInt(br.readLine()) - 1;
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int[] arrDistance = new int[cityCount];
+        int[] arrOilPrice = new int[cityCount];
+        int totalDistance = 0;
+        for (int i = 0; i < cityCount; i++) {
+            int tempDistance = Integer.parseInt(st1.nextToken());
+            arrDistance[i] = tempDistance;
+            totalDistance += tempDistance;
+            arrOilPrice[i] = Integer.parseInt(st2.nextToken());
+        }
+        int idx = 0;
+        int boughtDistance = arrDistance[0];
+        int currentOilPrice = arrOilPrice[0];
+        int boughtPrice = arrDistance[0] * currentOilPrice;
+        while (boughtDistance != totalDistance) {
+            if (currentOilPrice > arrOilPrice[++idx]) {
+                currentOilPrice = arrOilPrice[idx];
+            }
+            boughtPrice += arrDistance[idx] * currentOilPrice;
+            boughtDistance += arrDistance[idx];
+        }
+        System.out.println(boughtPrice);
+    }
+}
