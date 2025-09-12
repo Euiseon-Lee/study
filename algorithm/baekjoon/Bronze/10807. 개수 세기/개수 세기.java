e0f9653fec65ebaf5419d2d7a27f1e5d352224da
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-12 21:46:02 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14300 KB -EuiseonHub

---
+++ algorithm/baekjoon/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int target = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            int temp = Integer.parseInt(st.nextToken());
+            if (temp == target) answer++;
+        }
+        System.out.println(answer);
+    }
+}
