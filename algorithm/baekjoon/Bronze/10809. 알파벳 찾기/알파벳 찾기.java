01ef774ddb1431068b275cd1554b12ed05984eca
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-18 23:30:30 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 14328 KB -EuiseonHub

--- algorithm/baekjoon/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ algorithm/baekjoon/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,24 @@
 import java.io.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
         int[] alphabet = new int[26];
-        for (int i = 0; i < alphabet.length; i++) {
-            alphabet[i] = -1;
+        Arrays.fill(alphabet, -1);
+
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            int idx = arr[i] - 'a';
+            if (alphabet[idx] == -1) alphabet[idx] = i;
         }
-        String str = br.readLine();
-        for (int i = 0; i < str.length(); i++) {
-            int idx = str.charAt(i) - 'a';
-            if (alphabet[idx] == -1) {
-                alphabet[idx] = i;
-            }
-        }
+
         StringBuilder sb = new StringBuilder();
-        for (int a : alphabet) {
-            sb.append(a).append(" ");
+        for (int i = 0; i < alphabet.length; i++) {
+            sb.append(alphabet[i]);
+            if (i < 25) sb.append(' ');
         }
         System.out.print(sb);
     }
