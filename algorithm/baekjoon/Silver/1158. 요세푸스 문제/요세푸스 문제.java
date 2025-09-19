3566ee7b16e756c18fe3912400c520d26433ffaa
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-19 23:01:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 436 ms, Memory: 293880 KB -EuiseonHub

--- algorithm/baekjoon/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ algorithm/baekjoon/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -9,19 +9,19 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int removeIndex = Integer.parseInt(st.nextToken());
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = n; i >= 1; i--) {
-            stack.push(i);
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            queue.addLast(i);
         }
         StringBuilder sb = new StringBuilder("<");
-        while (stack.size() > 1) {
+        while (queue.size() > 1) {
             for (int i = 0; i < removeIndex - 1; i++) {
-                int val = stack.pollFirst();
-                stack.addLast(val);
+                int val = queue.pollFirst();
+                queue.addLast(val);
             }
-            sb.append(stack.pollFirst()).append(", ");
+            sb.append(queue.pollFirst()).append(", ");
         }
-        sb.append(stack.pollFirst()).append(">");
+        sb.append(queue.pollFirst()).append(">");
         System.out.println(sb);
     }
 }
