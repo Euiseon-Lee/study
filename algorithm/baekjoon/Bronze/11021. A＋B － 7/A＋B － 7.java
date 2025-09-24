fcd95df5fd362ad0939cd22ac2032e9d027cecbd
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-24 23:01:08 +0900

    [Bronze V] Title: A+B - 7, Time: 116 ms, Memory: 14448 KB -EuiseonHub

---
+++ algorithm/baekjoon/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            sb.append("Case #"+ (i + 1) + ": ").append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
