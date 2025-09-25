64d064ab5900ae3b69be3af0464ce4e6014408c5
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-25 22:13:23 +0900

    [Silver II] Title: 싸이버개강총회, Time: 476 ms, Memory: 51968 KB -EuiseonHub

---
+++ algorithm/baekjoon/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken().replace(":",""));
+        int e = Integer.parseInt(st.nextToken().replace(":",""));
+        int q = Integer.parseInt(st.nextToken().replace(":",""));
+        String line;
+        HashSet<String> set = new HashSet();
+        int count = 0;
+        while ((line = br.readLine()) != null) {
+            st = new StringTokenizer(line);
+            int time = Integer.parseInt(st.nextToken().replace(":",""));
+            String name = st.nextToken();
+            if (time <= s) set.add(name);
+            if (time >= e && time <= q && set.contains(name)) {
+                count++;
+                set.remove(name);
+            }
+        }
+        System.out.println(count);
+    }
+}
