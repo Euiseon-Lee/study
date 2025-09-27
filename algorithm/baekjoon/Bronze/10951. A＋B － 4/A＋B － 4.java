b6c3260a736078e90ace05832365bc5e728081db
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-27 23:49:27 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 14064 KB -EuiseonHub

---
+++ algorithm/baekjoon/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        StringTokenizer st;
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str);
+            System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())); ;
+        }
+    }
+}
