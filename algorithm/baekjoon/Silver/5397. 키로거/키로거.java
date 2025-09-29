df0ec60bc87cb7491280a676e2d5f81dc2ec9725
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-09-29 22:33:09 +0900

    [Silver II] Title: 키로거, Time: 664 ms, Memory: 120820 KB -EuiseonHub

--- algorithm/baekjoon/Silver/5397. 키로거/키로거.java
+++ algorithm/baekjoon/Silver/5397. 키로거/키로거.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Deque left = new ArrayDeque();
-        Deque right = new ArrayDeque();
-        StringBuffer sb = new StringBuffer();
+        Deque<Character> left = new ArrayDeque<>();
+        Deque<Character> right = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < str.length(); j++) {
