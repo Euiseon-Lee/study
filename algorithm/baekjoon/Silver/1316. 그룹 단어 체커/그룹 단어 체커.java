49e86184b85262c133ca2b9a6e14eff344c9655c
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-10-02 20:00:14 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14192 KB -EuiseonHub

--- algorithm/baekjoon/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ algorithm/baekjoon/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,33 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int answer = 0;
+
         for (int i = 0; i < n; i++) {
             String word = br.readLine();
-            Map<Character, Boolean> map = new HashMap<>();
-            char chPrev = word.charAt(0);
-            map.put(chPrev, true);
-            boolean isRelay = true;
-            for (int j = 1; j < word.length(); j++) {
+            boolean[] used = new boolean[26];
+            char prev = 0;
+            boolean isGroupWord = true;
+
+            for (int j = 0; j < word.length(); j++) {
                 char ch = word.charAt(j);
-                if (chPrev != ch) {
-                    if (map.containsKey(ch)) {
-                        isRelay = false;
+                if (ch != prev) {
+                    if (used[ch - 'a']) {
+                        isGroupWord = false;
                         break;
-                    } else {
-                        chPrev = ch;
-                        map.put(ch, true);
                     }
+                    used[ch - 'a'] = true;
+                    prev = ch;
                 }
             }
-            if (isRelay) answer++;
+            if (isGroupWord) answer++;
         }
         System.out.println(answer);
     }
