cc20a556232ee06a2e97406a0a27ed9050f2c3cb
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-10-19 20:52:52 +0900

    [Silver IV] Title: 듣보잡, Time: 248 ms, Memory: 26140 KB -EuiseonHub

---
+++ algorithm/baekjoon/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), 1);
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String s = br.readLine();
+            if (map.containsKey(s)) {
+                list.add(s);
+            }
+        }
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder();
+            sb.append(list.size()).append("\n");
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
