37d654b73fb3c30a7c2075118609337d29debd2b
Euiseon <97834419+Euiseon-Lee@users.noreply.github.com>
2025-11-07 23:54:18 +0900

    [Bronze II] Title: 카카오뷰 큐레이팅 효용성 분석, Time: 108 ms, Memory: 14740 KB -EuiseonHub

---
+++ algorithm/baekjoon/Bronze/24544. 카카오뷰 큐레이팅 효용성 분석/카카오뷰 큐레이팅 효용성 분석.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int total = 0;
+        int unregistered = 0;
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int content = Integer.parseInt(st1.nextToken());
+            boolean isRegistered = Integer.parseInt(st2.nextToken()) == 1 ? true : false;
+            total += content;
+            if (!isRegistered) unregistered += content;
+        }
+        System.out.println(total);
+        System.out.print(unregistered);
+    }
+}
